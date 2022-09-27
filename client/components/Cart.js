b7ba37f7fc27c8d65667caf85636c60055292def
Jordy <jordynavalee@gmail.com>
2022-09-27 16:37:44 -0500

    resolves merge conflicts'

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -19,86 +19,82 @@ export function Cart(props) {
   let products = orders?.products || [];
   let { user } = props;
 
-    return (
-      <div className="cart">
-        <h1>{user.firstName}'s Shopping Bag</h1>
-        <hr />
-        <div className="cartInfo">
-          <div className="productsInCart">
-            {products
-              ? products.map((product) => {
-                  return (
-                    <div key={product.id}>
-                      <div className="product">
-                        <img src={product.imageUrl} alt="image" />
-                        <div className="productInfo">
-                          <h2> {product.name}</h2>
-                        </div>
-                        <div className="price">
-                          <h3>
-                            Item Price: ${(product.price / 100).toFixed(2)}
-                          </h3>
-                        </div>
-                        <div className="quantity">
-                          <button
-                            onClick={() => {
-                              let quantity =
-                                (product.order_products.quantity -= 1);
-                              let orderId = orders.id;
-                              let productId = product.id;
-                              this.props.updateOrdersProduct({
-                                quantity,
-                                orderId,
-                                productId,
-                              });
-                              window.location.reload(false);
-                            }}
-                          >
-                            -
-                          </button>
-                          <h3>Quantity: {product.order_products.quantity}</h3>
+  return (
+    <div className="cart">
+      <h1>{user.firstName}'s Shopping Bag</h1>
+      <hr />
+      <div className="cartInfo">
+        <div className="productsInCart">
+          {products
+            ? products.map((product) => {
+                return (
+                  <div key={product.id}>
+                    <div className="product">
+                      <img src={product.imageUrl} alt="image" />
+                      <div className="productInfo">
+                        <h2> {product.name}</h2>
+                      </div>
+                      <div className="price">
+                        <h3>Item Price: ${(product.price / 100).toFixed(2)}</h3>
+                      </div>
+                      <div className="quantity">
+                        <button
+                          onClick={() => {
+                            let quantity =
+                              (product.order_products.quantity -= 1);
+                            let orderId = orders.id;
+                            let productId = product.id;
+                            props.updateOrdersProduct({
+                              quantity,
+                              orderId,
+                              productId,
+                            });
+                            window.location.reload(false);
+                          }}
+                        >
+                          -
+                        </button>
+                        <h3>Quantity: {product.order_products.quantity}</h3>
 
-                          <button
-                            onClick={() => {
-                              let quantity =
-                                (product.order_products.quantity += 1);
-                              let orderId = orders.id;
-                              let productId = product.id;
-                              this.props.updateOrdersProduct({
-                                quantity,
-                                orderId,
-                                productId,
-                              });
-                              window.location.reload(true);
-                            }}
-                          >
-                            +
-                          </button>
-                        </div>
-                        <div className="price">
-                          <h3>
-                            Total Price: $
-                            {(
-                              (product.price *
-                                product.order_products.quantity) /
-                              100
-                            ).toFixed(2)}
-                          </h3>
-                          <h5
-                            className="remove"
-                            onClick={() => {
-                              let orderId = orders.id;
-                              let productId = product.id;
-                              this.props.deleteOrderProduct({
-                                orderId,
-                                productId,
-                              });
-                              window.location.reload(false);
-                            }}
-                          >
-                            Remove
-                          </h5>
-                        </div>
+                        <button
+                          onClick={() => {
+                            let quantity =
+                              (product.order_products.quantity += 1);
+                            let orderId = orders.id;
+                            let productId = product.id;
+                            props.updateOrdersProduct({
+                              quantity,
+                              orderId,
+                              productId,
+                            });
+                            window.location.reload(true);
+                          }}
+                        >
+                          +
+                        </button>
+                      </div>
+                      <div className="price">
+                        <h3>
+                          Total Price: $
+                          {(
+                            (product.price * product.order_products.quantity) /
+                            100
+                          ).toFixed(2)}
+                        </h3>
+                        <h5
+                          className="remove"
+                          onClick={() => {
+                            let orderId = orders.id;
+                            let productId = product.id;
+                            props.deleteOrderProduct({
+                              orderId,
+                              productId,
+                            });
+                            window.location.reload(false);
+                          }}
+                        >
+                          Remove
+                        </h5>
                       </div>
                     </div>
                   </div>
@@ -113,7 +109,8 @@ export function Cart(props) {
             <button className="checkout">Proceed To Checkout</button>
           </Link>
         </div>
-        </div>
+      </div>
+    </div>
   );
 }
 
